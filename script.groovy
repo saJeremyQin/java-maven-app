b375b20d9441468f15521a027bfe7d02c6c3a595
Jeremy <343220868@qq.com>
2026-07-07 19:34:57 +0930

    update to github-token

--- script.groovy
+++ script.groovy
@@ -40,7 +40,7 @@ def commitBackToGit() {
     echo "committing back to git, in the function..."
     sh "git config user.email 'jenkins-bot@local'"
     sh 'git config user.name "jenkins-bot"'
-    withCredentials([string(credentialsId: 'jenkins-github-token', variable: 'GITHUB_TOKEN')
+    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')
     ]) {
         sh '''
             git add .
